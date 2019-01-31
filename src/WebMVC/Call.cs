53bc77f7e3746836f05553f019058ae7f31249c1
Rapidcognition <37350380+Rapidcognition@users.noreply.github.com>
2019-01-31 14:50:25 +0100

    changed name to webapp

---
+++ src/WebMVC/Call.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace WebMVC
+{
+    public class Call
+    {
+        public string Sid { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public string CallSegmentSid { get; set; }
+        public string AccountSid { get; set; }
+        public string Called { get; set; }
+        public string Caller { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
---
+++ src/WebMVC/LudoAPI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace WebApp
+{
+    public class LudoAPI
+    {
+        // example code
+        private const string BaseUrl = "https://ludogame.azurewebsites.net/api/ludo/";
+
+        public LudoAPI()
+        {
+            var client = new RestClient(BaseUrl);
+            var request = new RestRequest("{id}", Method.POST);
+            request.AddUrlSegment("id", "1323");
+        }
+    }
+}
