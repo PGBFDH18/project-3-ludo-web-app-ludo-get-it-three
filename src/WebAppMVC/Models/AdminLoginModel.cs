6a261571e05acd5e2497f27106e088a8a5d00d06
FkkLagom <larsson.benjamin@live.se>
2019-02-15 04:42:28 +0100

    Small fix

--- src/WebAppMVC/Models/AdminLoginModel.cs
+++ src/WebAppMVC/Models/AdminLoginModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,10 @@ namespace WebAppMVC.Models
 {
     public class AdminLoginModel
     {
+        [Required]
         public String Password { get; set; }
+
+        [Required]
         public String Username { get; set; }
     }
 }
