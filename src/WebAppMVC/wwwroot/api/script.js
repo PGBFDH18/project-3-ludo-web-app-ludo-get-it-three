24594a79762e86b986bd80e2ab4f7d1f5a1fee3d
Samuel Drysén <samuel.drysen@live.se>
2019-02-20 11:20:31 +0100

    Revert "Temporary revert see if fix maybe"
    This reverts commit 8e6f6f88afc8fa021f9cd79156894cd4880d4a04.

--- src/WebAppMVC/wwwroot/api/script.js
+++ src/WebAppMVC/wwwroot/api/script.js
@@ -1,9 +1,6 @@
 var localURL = "https://ludogame.azurewebsites.net/api/ludo/";
 var board = document.getElementById('boardDiv');
-<<<<<<< HEAD
 var rField = document.getElementById('rightField').getElementsByTagName('p1');
-=======
->>>>>>> parent of 9c39815... Fixes
 var newDiv = document.getElementById('newDiv');
 var message = document.getElementById('errorMessage');
 var gameStarted = document.getElementById("showCode");
@@ -149,8 +146,8 @@ function rollDice(gameURL) {
         }
     });
     diceValue = result;
-    rollNumber.textContent = result;
-    return board.appendChild(rollNumber);
+    rollNumber.textContent = "You rolled: " + result;
+    return rField.appendChild(rollNumber);
 }
 
 function startGame(gameURL) {
@@ -191,7 +188,7 @@ function getPosition(gameURL) {
 
     rollNumber2.textContent = output;
     
-    return board.appendChild(rollNumber2);
+    return rField.appendChild(rollNumber2);
 }
 
 function movePiece(gameURL){
