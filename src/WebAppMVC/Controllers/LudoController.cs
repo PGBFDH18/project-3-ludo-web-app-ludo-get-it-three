d3ecf59533caba5078bcee55bffddfa1a5fd7dfa
Samuel Drysén <samuel.drysen@live.se>
2019-02-24 16:10:15 +0100

    Implemented logging

--- src/WebAppMVC/Controllers/LudoController.cs
+++ src/WebAppMVC/Controllers/LudoController.cs
@@ -9,40 +9,48 @@ using Microsoft.AspNetCore.Http;
 using RestSharp;
 using Newtonsoft.Json;
 using WebAppMVC.Code;
+using Microsoft.Extensions.Logging;
 
 namespace WebAppMVC.Controllers
 {
     public class LudoController : Controller
     {
         private IRestClient client;
+        private ILogger<LudoController> logger;
 
-        public LudoController(IRestClient client)
+        public LudoController(IRestClient client, ILogger<LudoController> _logger)
         {
             this.client = client;
             client.BaseUrl = new Uri("https://ludogame.azurewebsites.net");
+            logger = _logger;
         }
 
         public IActionResult CreateGame(CreateGameModel model)
         {
             // Create game
             model.GameId = ApiMethods.CreateGame(client);
+            logger.LogInformation("User created a new game.");
 
             // Add all players that aren't null to the game.
             if (model.Player1Name != null)
             {
                 ApiMethods.AddPlayer(model.GameId, model.Player1Name, "0", client);
+                logger.LogInformation($"User added player: {model.Player1Name}");
             }
             if (model.Player2Name != null)
             {
                 ApiMethods.AddPlayer(model.GameId, model.Player2Name, "1", client);
+                logger.LogInformation($"User added player: {model.Player2Name}");
             }
             if (model.Player3Name != null)
             {
                 ApiMethods.AddPlayer(model.GameId, model.Player3Name, "2", client);
+                logger.LogInformation($"User added player: {model.Player3Name}");
             }
             if (model.Player4Name != null)
             {
                 ApiMethods.AddPlayer(model.GameId, model.Player4Name, "3", client);
+                logger.LogInformation($"User added player: {model.Player4Name}");
             }
 
             // Set cookies
@@ -82,6 +90,7 @@ namespace WebAppMVC.Controllers
         {
             if (Request.Cookies["gameid"] == null)
             {
+                logger.LogInformation("User tried to navigate to lobby with no assigned game.");
                 ViewBag.message = "You are currently not assigned to a game.";
                 return View();
             }
@@ -103,8 +112,11 @@ namespace WebAppMVC.Controllers
         {
             Guid gameId = Guid.Parse(Request.Cookies["gameid"]);
             ApiMethods.MovePiece(gameId, (model.pieceId - 1 ), int.Parse(Request.Cookies["lastdicevalue"]), client);
+            logger.LogInformation($"Piece moved number of steps: {Request.Cookies["lastdicevalue"].ToString()}");
             Response.Cookies.Append("lastdicevalue", "0");
             TempData["rolled"] = "false";
+
+            
             return RedirectToAction("game");
         }
 
@@ -121,6 +133,7 @@ namespace WebAppMVC.Controllers
                 CookieOptions cookie = new CookieOptions();
                 cookie.Expires = DateTime.Now.AddHours(1);
                 Response.Cookies.Append("lastdicevalue", ApiMethods.RollDice(gameId, client).ToString(), cookie);
+                logger.LogInformation("User rolled dice.");
             }
             TempData["rolled"] = "true";
             return RedirectToAction("game");
@@ -150,10 +163,12 @@ namespace WebAppMVC.Controllers
 
             if (startGameSuccess)
             {
+                logger.LogInformation("User successfully started game.");
                 return RedirectToAction("Game");
             }
             else
             {
+                logger.LogInformation("Failed to start game.");
                 ViewBag.errorMessage = "Couldn't start game.";
                 return View("Lobby", new PlayerModelContainer { gameId = gameId, Players = ApiMethods.GetAllPlayers(gameId, client) });
             }
